3ce869c2826859767be25befdc25bf75450fefec
Caret <wcle3456@gmail.com>
2020-01-08 16:06:30 +0900

    Add Splash Activity

---
+++ app/src/main/java/com/name/important/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.name.important;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 1500);
+    }
+}
